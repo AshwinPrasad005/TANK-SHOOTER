5695c93701dce6b7d1ba305480b01ddb9fd02974
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-16 19:25:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,11 +14,15 @@ var ground;
 
 function setup() {
     // Setup the canvas, the ground the, tanker, the shooting ball and the bubble balls.
-    var canvas = createCanvas(400,400);
+    var canvas = createCanvas(600,600);
 
-    background("white");
+    background(80,80,80);
+
+    engine = Engine.create();
+    world = engine.world;
 
     ground = new Ground(200,375,400,50);
+    ground.shapeColor = "green";
 }
 
 function draw() {
