b8ac31ff071657392989311413313dd5c37cf1b1
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-23 12:09:40 +0530

    Add files via upload

--- Tanker.js
+++ Tanker.js
@@ -3,11 +3,23 @@ class Tanker {
       var options = {
       isStatic: true
      }
+     this.body = Bodies.rectangle(x,y,width,height,options);
+     this.width = width;
+     this.height = height;
+     World.add(world,this.body)
+
     };
 
     display(){
       // Draw the tanker the way you like.
       // You could also use an image if you want a specific look
-
+      var pos = this.body.position;
+        //var angle = this.body.angle;
+        push();
+        //translate(this.body.position.x,this.body.position.y);
+        rectMode(CENTER);
+        //rotate(angle);
+        rect(pos.x,pos.y,this.width,this.height);
+        pop();
     };
 }
